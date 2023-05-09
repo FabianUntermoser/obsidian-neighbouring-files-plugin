799eaaa662082f36735cf916cea46943adbc536d
Fabian Untermoser <fabian@ederbit.xyz>
2023-05-09 10:09:46 +0200

    chore: prettier format

--- main.ts
+++ main.ts
@@ -1,44 +1,45 @@
-import { Plugin, TFile } from 'obsidian';
+import { Plugin, TFile } from "obsidian";
 
 export default class NeighbouringFileNavigator extends Plugin {
-
 	getNeighbouringFiles(file: TFile) {
 		const files = file.parent?.children;
-		return files
+		return files;
 	}
 
 	navigateToNeighbouringFile(next?: boolean) {
 		const activeFile = this.app.workspace.getActiveFile();
 		const files = activeFile?.parent?.children;
-		files?.sort((a,b) => a.name > b.name ? 1 : -1)
-		if (!activeFile || !files) return
+		files?.sort((a, b) => (a.name > b.name ? 1 : -1));
+		if (!activeFile || !files) return;
 
-		const currentItem = files.findIndex(item => item.name === activeFile.name)
+		const currentItem = files.findIndex(
+			(item) => item.name === activeFile.name
+		);
 
-		const toFile = next ?
-			files[(currentItem+1)%files.length] :
-			files[currentItem == 0 ? files.length-1 : (currentItem-1)%files.length]
+		const toFile = next
+			? files[(currentItem + 1) % files.length]
+			: files[
+					currentItem == 0
+						? files.length - 1
+						: (currentItem - 1) % files.length
+			  ];
 
-		this.app.workspace.openLinkText(toFile.path, '', false);
+		this.app.workspace.openLinkText(toFile.path, "", false);
 	}
 
 	async onload() {
-
 		this.addCommand({
-			id: 'next',
-			name: 'Navigate to next file in folder',
-			callback: () => this.navigateToNeighbouringFile(true)
+			id: "next",
+			name: "Navigate to next file in folder",
+			callback: () => this.navigateToNeighbouringFile(true),
 		});
 
 		this.addCommand({
-			id: 'prev',
-			name: 'Navigate to previous file in folder',
-			callback: () => this.navigateToNeighbouringFile(false)
+			id: "prev",
+			name: "Navigate to previous file in folder",
+			callback: () => this.navigateToNeighbouringFile(false),
 		});
-
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 }
