d41c43e332014b81d0d6b609d99d4b0d9bcc4190
Fabian Untermoser <fabian@ederbit.xyz>
2024-02-08 22:07:14 +0100

    feat: change type to return array of files

--- src/NeighbouringFileNavigator.ts
+++ src/NeighbouringFileNavigator.ts
@@ -4,7 +4,7 @@ export default class NeighbouringFileNavigator {
 
 	public static getNeighbouringFiles(file: TFile) {
 		const files = file?.parent?.children;
-		const filteredFiles = files?.filter(f => f instanceof TFile && f.extension === 'md');
+		const filteredFiles = files?.filter(f => f instanceof TFile && f.extension === 'md') as TFile[];
 		const sortedFiles = filteredFiles?.sort((a, b) =>
 			a.name.localeCompare(b.name, undefined, {
 				numeric: true,
