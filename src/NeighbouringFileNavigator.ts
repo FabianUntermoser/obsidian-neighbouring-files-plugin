016b70cf9c9fb4c0ada69f62211326137ab3fdd1
Fabian Untermoser <fabian@ederbit.xyz>
2024-09-08 18:06:05 +0200

    refactor: move logic to class

--- src/NeighbouringFileNavigator.ts
+++ src/NeighbouringFileNavigator.ts
@@ -1,6 +1,28 @@
-import { TFile } from "obsidian";
+import { TFile, Workspace } from "obsidian";
 
 export default class NeighbouringFileNavigator {
+
+	public static navigateToNeighbouringFile(workspace: Workspace, next?: boolean) {
+		const activeFile = workspace.getActiveFile();
+		if (!activeFile) return;
+
+		const files = NeighbouringFileNavigator.getNeighbouringFiles(activeFile);
+		if (!files) return;
+
+		const currentItem = files.findIndex(
+			(item) => item.name === activeFile.name
+		);
+
+		const toFile = next
+			? files[(currentItem + 1) % files.length]
+			: files[
+			currentItem == 0
+				? files.length - 1
+				: (currentItem - 1) % files.length
+			];
+
+		workspace.getLeaf(false).openFile(toFile as TFile);
+	}
 
 	public static getNeighbouringFiles(file: TFile) {
 		const files = file?.parent?.children;
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,19 @@
 import NeighbouringFileNavigator from "NeighbouringFileNavigator";
-import { Plugin, TFile } from "obsidian";
+import { Plugin } from "obsidian";
 
 export default class NeighbouringFileNavigatorPlugin extends Plugin {
-
-	navigateToNeighbouringFile(next?: boolean) {
-		const activeFile = this.app.workspace.getActiveFile();
-		if (!activeFile) return;
-
-		const files = NeighbouringFileNavigator.getNeighbouringFiles(activeFile);
-		if (!files) return;
-
-		const currentItem = files.findIndex(
-			(item) => item.name === activeFile.name
-		);
-
-		const toFile = next
-			? files[(currentItem + 1) % files.length]
-			: files[
-			currentItem == 0
-				? files.length - 1
-				: (currentItem - 1) % files.length
-			];
-
-		this.app.workspace.getLeaf(false).openFile(toFile as TFile);
-	}
 
 	async onload() {
 		this.addCommand({
 			id: "next",
 			name: "Navigate to next file",
-			callback: () => this.navigateToNeighbouringFile(true),
+			callback: () => NeighbouringFileNavigator.navigateToNeighbouringFile(this.app.workspace, true),
 		});
 
 		this.addCommand({
 			id: "prev",
 			name: "Navigate to previous file",
-			callback: () => this.navigateToNeighbouringFile(false),
+			callback: () => NeighbouringFileNavigator.navigateToNeighbouringFile(this.app.workspace, false),
 		});
 	}
 
