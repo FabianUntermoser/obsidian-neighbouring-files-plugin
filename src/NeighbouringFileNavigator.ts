38451c964df165619323cce125af0d12cb6a0a48
Fabian Untermoser <fabian@ederbit.xyz>
2024-12-28 15:40:59 +0100

    feat: add cmds to navigate files with created and modified sorting

--- src/NeighbouringFileNavigator.ts
+++ src/NeighbouringFileNavigator.ts
@@ -12,33 +12,75 @@ export default class NeighbouringFileNavigator {
 		this.navigateToNeighbouringFile(workspace, false);
 	}
 
-	public static navigateToNextModifiedFile(workspace: Workspace) {
-		console.debug("navigateToNextModifiedFile");
-		// NOT IMPLEMENTED
-		this.navigateToNeighbouringFile(workspace, false);
-	}
-
-	public static navigateToPrevModifiedFile(workspace: Workspace) {
-		console.debug("navigateToPrevModifiedFile");
-		// NOT IMPLEMENTED
-	}
-
 	public static navigateToNextCreatedFile(workspace: Workspace) {
 		console.debug("navigateToNextCreatedFile");
-		// NOT IMPLEMENTED
+		this.navigateToNeighbouringCreatedFile(workspace, true);
 	}
 
 	public static navigateToPrevCreatedFile(workspace: Workspace) {
 		console.debug("navigateToPrevCreatedFile");
-		// NOT IMPLEMENTED
+		this.navigateToNeighbouringCreatedFile(workspace, false);
 	}
 
+	public static navigateToNextModifiedFile(workspace: Workspace) {
+		console.debug("navigateToNextModifiedFile");
+		this.navigateToNeighbouringModifiedFile(workspace, true);
+	}
+
+	public static navigateToPrevModifiedFile(workspace: Workspace) {
+		console.debug("navigateToPrevModifiedFile");
+		this.navigateToNeighbouringModifiedFile(workspace, false);
+	}
 
 	public static navigateToNeighbouringFile(workspace: Workspace, next?: boolean) {
 		const activeFile = workspace.getActiveFile();
 		if (!activeFile) return;
 
 		const files = NeighbouringFileNavigator.getNeighbouringFiles(activeFile);
+		if (!files) return;
+
+		const currentItem = files.findIndex(
+			(item) => item.name === activeFile.name
+		);
+
+		const toFile = next
+			? files[(currentItem + 1) % files.length]
+			: files[
+			currentItem == 0
+				? files.length - 1
+				: (currentItem - 1) % files.length
+			];
+
+		workspace.getLeaf(false).openFile(toFile as TFile);
+	}
+
+	public static navigateToNeighbouringCreatedFile(workspace: Workspace, next?: boolean) {
+		const activeFile = workspace.getActiveFile();
+		if (!activeFile) return;
+
+		const files = NeighbouringFileNavigator.getNeighbouringCreatedFiles(activeFile);
+		if (!files) return;
+
+		const currentItem = files.findIndex(
+			(item) => item.name === activeFile.name
+		);
+
+		const toFile = next
+			? files[(currentItem + 1) % files.length]
+			: files[
+			currentItem == 0
+				? files.length - 1
+				: (currentItem - 1) % files.length
+			];
+
+		workspace.getLeaf(false).openFile(toFile as TFile);
+	}
+
+	public static navigateToNeighbouringModifiedFile(workspace: Workspace, next?: boolean) {
+		const activeFile = workspace.getActiveFile();
+		if (!activeFile) return;
+
+		const files = NeighbouringFileNavigator.getNeighbouringModifiedFiles(activeFile);
 		if (!files) return;
 
 		const currentItem = files.findIndex(
@@ -64,6 +106,20 @@ export default class NeighbouringFileNavigator {
 				numeric: true,
 				sensitivity: 'base',
 			}))
+		return sortedFiles;
+	}
+
+	public static getNeighbouringCreatedFiles(file: TFile) {
+		const files = file?.parent?.children;
+		const filteredFiles = files?.filter(f => f instanceof TFile && f.extension === 'md') as TFile[];
+		const sortedFiles = filteredFiles?.sort((a, b) => { return a.stat.ctime - b.stat.ctime; });
+		return sortedFiles;
+	}
+
+	public static getNeighbouringModifiedFiles(file: TFile) {
+		const files = file?.parent?.children;
+		const filteredFiles = files?.filter(f => f instanceof TFile && f.extension === 'md') as TFile[];
+		const sortedFiles = filteredFiles?.sort((a, b) => { return a.stat.mtime - b.stat.mtime; });
 		return sortedFiles;
 	}
 
--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,32 @@ export default class NeighbouringFileNavigatorPlugin extends Plugin {
 			name: "Navigate to next file",
 			callback: () => NeighbouringFileNavigator.navigateToNextFile(this.app.workspace),
 		});
-
 		this.addCommand({
 			id: "prev",
 			name: "Navigate to previous file",
 			callback: () => NeighbouringFileNavigator.navigateToPrevFile(this.app.workspace),
+		});
+
+		this.addCommand({
+			id: "next-created",
+			name: "Navigate to next created file",
+			callback: () => NeighbouringFileNavigator.navigateToNextCreatedFile(this.app.workspace),
+		});
+		this.addCommand({
+			id: "prev-created",
+			name: "Navigate to previous created file",
+			callback: () => NeighbouringFileNavigator.navigateToPrevCreatedFile(this.app.workspace),
+		});
+
+		this.addCommand({
+			id: "next-modified",
+			name: "Navigate to next modified file",
+			callback: () => NeighbouringFileNavigator.navigateToNextModifiedFile(this.app.workspace),
+		});
+		this.addCommand({
+			id: "prev-modified",
+			name: "Navigate to previous modified file",
+			callback: () => NeighbouringFileNavigator.navigateToPrevModifiedFile(this.app.workspace),
 		});
 	}
 
