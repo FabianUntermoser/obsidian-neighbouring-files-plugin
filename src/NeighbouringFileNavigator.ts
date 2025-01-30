70e7c63da7feeee7a6341939ae1cf61ba685d8c7
Brendon Felix <brendon.felix@hp.com>
2025-01-30 12:35:09 -0700

    refactor reverse sorting

--- src/NeighbouringFileNavigator.ts
+++ src/NeighbouringFileNavigator.ts
@@ -11,27 +11,22 @@ export class NeighbouringFileNavigator {
 			undefined,
 			{ numeric: true, sensitivity: 'base' }
 		);
-	
-	public static localeSorterReverse: SortFn = (a: TFile, b: TFile) =>
-		b.basename.localeCompare(
-			a.basename,
-			undefined,
-			{ numeric: true, sensitivity: 'base' }
-		);
 
-	public static mtimeSorterReverse: SortFn = (a: TFile, b: TFile) => { return a.stat.mtime - b.stat.mtime; };
 	public static mtimeSorter: SortFn = (a: TFile, b: TFile) => { return b.stat.mtime - a.stat.mtime; };
 
-	public static ctimeSorterReverse: SortFn = (a: TFile, b: TFile) => { return a.stat.ctime - b.stat.ctime; };
 	public static ctimeSorter: SortFn = (a: TFile, b: TFile) => { return b.stat.ctime - a.stat.ctime; };
+	
+	public static reverse(fn: SortFn): SortFn {
+		return (a, b) => -fn(a, b);
+	}
 
 	static sorters: Record<SORT_ORDER, SortFn> = {
 		alphabetical: this.localeSorter,
-		alphabeticalReverse: this.localeSorterReverse,
+		alphabeticalReverse: this.reverse(this.localeSorter),
 		byCreatedTime: this.ctimeSorter,
-		byCreatedTimeReverse: this.ctimeSorterReverse,
+		byCreatedTimeReverse: this.reverse(this.ctimeSorter),
 		byModifiedTime: this.mtimeSorter,
-		byModifiedTimeReverse: this.mtimeSorterReverse,
+		byModifiedTimeReverse: this.reverse(this.mtimeSorter),
 	};
 
 	public static navigateToNextFile(workspace: Workspace, settings: NeighbouringFileNavigatorPluginSettings) {
@@ -39,7 +34,7 @@ export class NeighbouringFileNavigator {
 			?? settings.defaultSortOrder;
 		console.debug("navigateToNextFile with sort mode", sortOrder);
 		const sortFn = this.sorters[sortOrder];
-		this.navigateToNeighbouringFile(workspace, sortFn, true);
+		this.navigateToNeighbouringFile(workspace, sortFn);
 	}
 
 	public static navigateToPrevFile(workspace: Workspace, settings: NeighbouringFileNavigatorPluginSettings) {
@@ -47,40 +42,40 @@ export class NeighbouringFileNavigator {
 			?? settings.defaultSortOrder;
 		console.debug("navigateToPrevFile with sort mode", sortOrder);
 		const sortFn = this.sorters[sortOrder];
-		this.navigateToNeighbouringFile(workspace, sortFn, false);
+		this.navigateToNeighbouringFile(workspace, this.reverse(sortFn));
 	}
 
 	public static navigateToNextAlphabeticalFile(workspace: Workspace) {
 		console.debug("navigateToNextAlphabeticalFile");
-		this.navigateToNeighbouringFile(workspace, this.localeSorter, true);
+		this.navigateToNeighbouringFile(workspace, this.localeSorter);
 	}
 
 	public static navigateToPrevAlphabeticalFile(workspace: Workspace) {
 		console.debug("navigateToPrevAlphabeticalFile");
-		this.navigateToNeighbouringFile(workspace, this.localeSorter, false);
+		this.navigateToNeighbouringFile(workspace, this.reverse(this.localeSorter));
 	}
 
 	public static navigateToNextCreatedFile(workspace: Workspace) {
 		console.debug("navigateToNextCreatedFile");
-		this.navigateToNeighbouringFile(workspace, this.ctimeSorter, true);
+		this.navigateToNeighbouringFile(workspace, this.ctimeSorter);
 	}
 
 	public static navigateToPrevCreatedFile(workspace: Workspace) {
 		console.debug("navigateToPrevCreatedFile");
-		this.navigateToNeighbouringFile(workspace, this.ctimeSorter, false);
+		this.navigateToNeighbouringFile(workspace, this.reverse(this.ctimeSorter));
 	}
 
 	public static navigateToNextModifiedFile(workspace: Workspace) {
 		console.debug("navigateToNextModifiedFile");
-		this.navigateToNeighbouringFile(workspace, this.mtimeSorter, true);
+		this.navigateToNeighbouringFile(workspace, this.mtimeSorter);
 	}
 
 	public static navigateToPrevModifiedFile(workspace: Workspace) {
 		console.debug("navigateToPrevModifiedFile");
-		this.navigateToNeighbouringFile(workspace, this.mtimeSorter, false);
+		this.navigateToNeighbouringFile(workspace, this.reverse(this.mtimeSorter));
 	}
 
-	public static navigateToNeighbouringFile(workspace: Workspace, sortFn: SortFn, next?: boolean) {
+	public static navigateToNeighbouringFile(workspace: Workspace, sortFn: SortFn) {
 		const activeFile = workspace.getActiveFile();
 		if (!activeFile) return;
 
@@ -91,13 +86,7 @@ export class NeighbouringFileNavigator {
 			(item) => item.name === activeFile.name
 		);
 
-		const toFile = next
-			? files[(currentItem + 1) % files.length]
-			: files[
-			currentItem == 0
-				? files.length - 1
-				: (currentItem - 1) % files.length
-		];
+		const toFile = files[(currentItem + 1) % files.length];
 
 		workspace.getLeaf(false).openFile(toFile as TFile);
 	}
