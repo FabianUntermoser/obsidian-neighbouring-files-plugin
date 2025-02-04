1cd4621edd7339f8b35867e96c5860f36379d71f
Fabian Untermoser <fabian@ederbit.xyz>
2025-02-04 22:22:32 +0100

    refactor: use sorters

--- src/NeighbouringFileNavigator.ts
+++ src/NeighbouringFileNavigator.ts
@@ -15,17 +15,17 @@ export class NeighbouringFileNavigator {
 	public static mtimeSorter: SortFn = (a: TFile, b: TFile) => { return b.stat.mtime - a.stat.mtime; };
 
 	public static ctimeSorter: SortFn = (a: TFile, b: TFile) => { return b.stat.ctime - a.stat.ctime; };
-	
+
 	public static reverse(fn: SortFn): SortFn {
 		return (a, b) => -fn(a, b);
 	}
 
 	static sorters: Record<SORT_ORDER, SortFn> = {
 		alphabetical: this.localeSorter,
-		alphabeticalReverse: this.reverse(this.localeSorter),
 		byCreatedTime: this.ctimeSorter,
-		byCreatedTimeReverse: this.reverse(this.ctimeSorter),
 		byModifiedTime: this.mtimeSorter,
+		alphabeticalReverse: this.reverse(this.localeSorter),
+		byCreatedTimeReverse: this.reverse(this.ctimeSorter),
 		byModifiedTimeReverse: this.reverse(this.mtimeSorter),
 	};
 
@@ -50,32 +50,32 @@ export class NeighbouringFileNavigator {
 
 	public static navigateToNextAlphabeticalFile(workspace: Workspace) {
 		console.debug("navigateToNextAlphabeticalFile");
-		this.navigateToNeighbouringFile(workspace, this.localeSorter);
+		this.navigateToNeighbouringFile(workspace, this.sorters.alphabetical);
 	}
 
 	public static navigateToPrevAlphabeticalFile(workspace: Workspace) {
 		console.debug("navigateToPrevAlphabeticalFile");
-		this.navigateToNeighbouringFile(workspace, this.reverse(this.localeSorter));
+		this.navigateToNeighbouringFile(workspace, this.sorters.alphabeticalReverse);
 	}
 
 	public static navigateToNextCreatedFile(workspace: Workspace) {
 		console.debug("navigateToNextCreatedFile");
-		this.navigateToNeighbouringFile(workspace, this.ctimeSorter);
+		this.navigateToNeighbouringFile(workspace, this.sorters.byCreatedTime);
 	}
 
 	public static navigateToPrevCreatedFile(workspace: Workspace) {
 		console.debug("navigateToPrevCreatedFile");
-		this.navigateToNeighbouringFile(workspace, this.reverse(this.ctimeSorter));
+		this.navigateToNeighbouringFile(workspace, this.sorters.byCreatedTimeReverse);
 	}
 
 	public static navigateToNextModifiedFile(workspace: Workspace) {
 		console.debug("navigateToNextModifiedFile");
-		this.navigateToNeighbouringFile(workspace, this.mtimeSorter);
+		this.navigateToNeighbouringFile(workspace, this.sorters.byModifiedTime);
 	}
 
 	public static navigateToPrevModifiedFile(workspace: Workspace) {
 		console.debug("navigateToPrevModifiedFile");
-		this.navigateToNeighbouringFile(workspace, this.reverse(this.mtimeSorter));
+		this.navigateToNeighbouringFile(workspace, this.sorters.byModifiedTimeReverse);
 	}
 
 	public static navigateToNeighbouringFile(workspace: Workspace, sortFn: SortFn) {
