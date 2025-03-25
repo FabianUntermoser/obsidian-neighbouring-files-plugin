de54fe6ba6954b413b105f48b073024f1b58d6a4
Fabian Untermoser <fabian@ederbit.xyz>
2025-03-25 22:57:07 +0100

    test: restructure test sections

--- test/NeighbouringFileNavigator.test.ts
+++ test/NeighbouringFileNavigator.test.ts
@@ -4,12 +4,16 @@ import { FileStats, TAbstractFile, TFile, TFolder } from "obsidian";
 
 const createNote = (name: string, stats?: FileStats): TFile => createFile(name, "md", stats);
 
-const createFile = (name: string, extension: string, stats?: FileStats): TFile => {
+const createFile = (
+	name: string,
+	extension: string,
+	stats?: FileStats
+): TFile => {
 	const f = new TFile();
 	f.basename = name;
 	f.extension = extension;
 	f.name = `${name}.${extension}`;
-	f.stat = stats ?? { ctime: 1, mtime: 1, size: 1, };
+	f.stat = stats ?? { ctime: 1, mtime: 1, size: 1 };
 	return f;
 };
 
@@ -17,25 +21,25 @@ const createDir = (name: string): TFolder => {
 	const dir = new TFolder();
 	dir.name = name;
 	dir.children = [];
-	return dir
+	return dir;
 };
 
 const setup = (children: Array<TAbstractFile>) => {
 	const parent = new TFolder();
 	parent.children = children;
-	children.forEach(c => c.parent = parent);
+	children.forEach((c) => (c.parent = parent));
 	return children;
 };
 
 const setupFiles = (names: Array<string>) => {
-	const children = names.map(c => createNote(c));
+	const children = names.map((c) => createNote(c));
 	return setup(children) as TFile[];
 };
 
-const expectNeighbours = (files: Array<TFile> ) => {
-	const names = files?.map(n => n.basename)
-	return expect(names)
-}
+const expectNeighbours = (files: Array<TFile>) => {
+	const names = files?.map((n) => n.basename);
+	return expect(names);
+};
 
 describe("NeighbouringFileNavigator", () => {
 	const settings: NeighbouringFileNavigatorPluginSettings = DEFAULT_SETTINGS;
@@ -50,255 +54,260 @@ describe("NeighbouringFileNavigator", () => {
 
 	beforeEach(() => {
 		jest.clearAllMocks();
-	})
-
-	it("should contain all files", () => {
-		// GIVEN
-		const files = setupFiles(["1", "2", "3"]);
-
-		// WHEN
-		const neighbours = navigator.getNeighbouringFiles(files[0], NeighbouringFileNavigator.localeSorter);
-
-		// THEN
-		expect(neighbours).toHaveLength(3);
-		files.forEach((child) => expect(neighbours).toContain(child));
 	});
 
-	it("should only filter for markdown files", () => {
-		// GIVEN
-		const files = setup([
-			createNote("1"),
-			createNote("2"),
-			createFile("3", "pdf"),
-		]) as TFile[];
+	describe("Sorting", () => {
+		it("should contain all files", () => {
+			// GIVEN
+			const files = setupFiles(["1", "2", "3"]);
 
-		// WHEN
-		const neighbours = navigator.getNeighbouringFiles(
-			files[0],
-			NeighbouringFileNavigator.localeSorter
-		);
+			// WHEN
+			const neighbours = navigator.getNeighbouringFiles(
+				files[0],
+				NeighbouringFileNavigator.localeSorter
+			);
 
-		// THEN
-		expect(neighbours).toHaveLength(2);
-		expect(neighbours).toContain(files[0]);
-		expect(neighbours).toContain(files[1]);
+			// THEN
+			expect(neighbours).toHaveLength(3);
+			files.forEach((child) => expect(neighbours).toContain(child));
+		});
+
+		it("should only filter for markdown files", () => {
+			// GIVEN
+			const files = setup([
+				createNote("1"),
+				createNote("2"),
+				createFile("3", "pdf"),
+			]) as TFile[];
+
+			// WHEN
+			const neighbours = navigator.getNeighbouringFiles(
+				files[0],
+				NeighbouringFileNavigator.localeSorter
+			);
+
+			// THEN
+			expect(neighbours).toHaveLength(2);
+			expect(neighbours).toContain(files[0]);
+			expect(neighbours).toContain(files[1]);
+		});
+
+		it("should filter out directories", () => {
+			// GIVEN
+			const files = setup([
+				createNote("1"),
+				createDir("somedir"),
+			]) as TFile[];
+
+			// WHEN
+			const neighbours = navigator.getNeighbouringFiles(
+				files[0],
+				NeighbouringFileNavigator.localeSorter
+			);
+
+			// THEN
+			expect(neighbours).toHaveLength(1);
+			expect(neighbours).toContain(files[0]);
+		});
+
+		it("should sort files", () => {
+			// GIVEN
+			const files = setupFiles(["2", "1", "3"]);
+
+			// WHEN
+			const neighbours = navigator.getNeighbouringFiles(
+				files[0],
+				NeighbouringFileNavigator.localeSorter
+			);
+
+			// THEN
+			expectNeighbours(neighbours).toEqual(["1", "2", "3"]);
+		});
+
+		it("should sort ignoring case", () => {
+			// GIVEN
+			const files = setupFiles(["test - 3", "Test - 2", "test - 1"]);
+
+			// WHEN
+			const neighbours = navigator.getNeighbouringFiles(
+				files[0],
+				NeighbouringFileNavigator.localeSorter
+			);
+
+			// THEN
+			expectNeighbours(neighbours).toEqual([
+				"test - 1",
+				"Test - 2",
+				"test - 3",
+			]);
+		});
+
+		it("should sort johny decimal", () => {
+			// GIVEN
+			const files = setupFiles([
+				"2.2",
+				"1",
+				"2.9",
+				"2.1",
+				"2",
+				"3",
+				"2.11",
+				"2.10",
+			]);
+
+			// WHEN
+			const neighbours = navigator.getNeighbouringFiles(
+				files[0],
+				NeighbouringFileNavigator.localeSorter
+			);
+
+			// THEN
+			expectNeighbours(neighbours).toEqual([
+				"1",
+				"2",
+				"2.1",
+				"2.2",
+				"2.9",
+				"2.10",
+				"2.11",
+				"3",
+			]);
+		});
+
+		it("should sort files based on creation timestamp", () => {
+			// GIVEN
+			const files = setup([
+				createNote("2", {
+					ctime: 1689876543210, // 2023-07-20T01:22:23.210Z
+					mtime: 1704025483489, // 2023-12-31T12:24:43.489Z
+					size: 4380, // 4.38 KB
+				}),
+				createNote("1", {
+					ctime: 1700989701724, // 2023-11-26T04:01:41.724Z
+					mtime: 1692456789100, // 2023-08-19T11:13:09.100Z
+					size: 102400, // 100 KB
+				}),
+				createNote("3", {
+					ctime: 1672502400000, // 2023-01-01T00:00:00.000Z
+					mtime: 1675180800000, // 2023-02-01T00:00:00.000Z
+					size: 5242880, // 5 MB
+				}),
+			]) as TFile[];
+
+			// WHEN
+			const neighbours = navigator.getNeighbouringFiles(
+				files[0],
+				NeighbouringFileNavigator.ctimeSorter
+			);
+
+			// THEN
+			expectNeighbours(neighbours).toEqual(["1", "2", "3"]);
+		});
+
+		it("should sort files based on modified timestamp", () => {
+			// GIVEN
+			const files = setup([
+				createNote("2", {
+					ctime: 1700989701724, // 2023-11-26T04:01:41.724Z
+					mtime: 1692456789100, // 2023-08-19T11:13:09.100Z
+					size: 5242880, // 5 MB
+				}),
+				createNote("3", {
+					ctime: 1689876543210, // 2023-07-20T01:22:23.210Z
+					mtime: 1675180800000, // 2023-02-01T00:00:00.000Z
+					size: 102400, // 100 KB
+				}),
+				createNote("1", {
+					ctime: 1672502400000, // 2023-01-01T00:00:00.000Z
+					mtime: 1704025483489, // 2023-12-31T12:24:43.489Z
+					size: 4380, // 4.38 KB
+				}),
+			]) as TFile[];
+
+			// WHEN
+			const neighbours = navigator.getNeighbouringFiles(
+				files[0],
+				NeighbouringFileNavigator.mtimeSorter
+			);
+
+			// THEN
+			expectNeighbours(neighbours).toEqual(["1", "2", "3"]);
+		});
 	});
 
-	it("should filter out directories", () => {
-		// GIVEN
-		const files = setup([createNote("1"), createDir("somedir")]) as TFile[];
+	describe("Settings: Folder Loop", () => {
+		it("should loop folder", () => {
+			// GIVEN
+			const files = setupFiles(["1", "2", "3"]);
+			workspace.getActiveFile.mockReturnValue(files[2]);
+			settings.enableFolderLoop = true;
 
-		// WHEN
-		const neighbours = navigator.getNeighbouringFiles(
-			files[0],
-			NeighbouringFileNavigator.localeSorter
-		);
+			// WHEN
+			navigator.navigateToNextAlphabeticalFile(workspace);
 
-		// THEN
-		expect(neighbours).toHaveLength(1);
-		expect(neighbours).toContain(files[0]);
+			// THEN
+			expect(leaf.openFile).toHaveBeenCalledWith(files[0]);
+			expect(leaf.openFile).not.toHaveBeenCalledWith(files[1]);
+			expect(leaf.openFile).not.toHaveBeenCalledWith(files[2]);
+		});
+
+		it("should not loop folders", () => {
+			// GIVEN
+			const files = setupFiles(["1", "2", "3"]);
+			workspace.getActiveFile.mockReturnValue(files[2]);
+			settings.enableFolderLoop = false;
+
+			// WHEN
+			navigator.navigateToNextAlphabeticalFile(workspace);
+
+			// THEN
+			expect(leaf.openFile).toHaveBeenCalledWith(files[2]);
+			expect(leaf.openFile).not.toHaveBeenCalledWith(files[0]);
+			expect(leaf.openFile).not.toHaveBeenCalledWith(files[1]);
+		});
 	});
 
-	it("should sort files", () => {
-		// GIVEN
-		const files = setupFiles(["2", "1", "3"]);
+	describe("Settings: File Types", () => {
+		it("should include specified extensions", () => {
+			// GIVEN
+			settings.includedFileTypes = "additionalExtensions";
+			settings.additionalExtensions = ["pdf"];
 
-		// WHEN
-		const neighbours = navigator.getNeighbouringFiles(
-			files[0],
-			NeighbouringFileNavigator.localeSorter
-		);
+			const files = setup([
+				createNote("1"),
+				createNote("2"),
+				createFile("3", "txt"),
+				createFile("4", "pdf"),
+			]);
 
-		// THEN
-		expectNeighbours(neighbours).toEqual(["1", "2", "3"]);
+			// WHEN
+			const neighbours = navigator.getNeighbouringFiles(
+				files[0] as TFile,
+				NeighbouringFileNavigator.localeSorter
+			);
+
+			// THEN
+			expectNeighbours(neighbours).toEqual(["1", "2", "4"]);
+		});
+
+		it("should include all files", () => {
+			// GIVEN
+			settings.includedFileTypes = "allFiles";
+			const files = setup([
+				createNote("1"),
+				createNote("2"),
+				createFile("3", "txt"),
+				createFile("4", "pdf"),
+			]);
+
+			// WHEN
+			const neighbours = navigator.getNeighbouringFiles(
+				files[0] as TFile,
+				NeighbouringFileNavigator.localeSorter
+			);
+
+			// THEN
+			expectNeighbours(neighbours).toEqual(["1", "2", "3", "4"]);
+		});
 	});
-
-	it("should sort ignoring case", () => {
-		// GIVEN
-		const files = setupFiles(["test - 3", "Test - 2", "test - 1"]);
-
-		// WHEN
-		const neighbours = navigator.getNeighbouringFiles(
-			files[0],
-			NeighbouringFileNavigator.localeSorter
-		);
-
-		// THEN
-		expectNeighbours(neighbours).toEqual([
-			"test - 1",
-			"Test - 2",
-			"test - 3",
-		]);
-	});
-
-	it("should sort johny decimal", () => {
-		// GIVEN
-		const files = setupFiles([
-			"2.2",
-			"1",
-			"2.9",
-			"2.1",
-			"2",
-			"3",
-			"2.11",
-			"2.10",
-		]);
-
-		// WHEN
-		const neighbours = navigator.getNeighbouringFiles(
-			files[0],
-			NeighbouringFileNavigator.localeSorter
-		);
-
-		// THEN
-		expectNeighbours(neighbours).toEqual([
-			"1",
-			"2",
-			"2.1",
-			"2.2",
-			"2.9",
-			"2.10",
-			"2.11",
-			"3",
-		]);
-	});
-
-	it("should sort files based on creation timestamp", () => {
-		// GIVEN
-		const files = setup([
-			createNote("2", {
-				ctime: 1689876543210, // 2023-07-20T01:22:23.210Z
-				mtime: 1704025483489, // 2023-12-31T12:24:43.489Z
-				size: 4380, // 4.38 KB
-			}),
-			createNote("1", {
-				ctime: 1700989701724, // 2023-11-26T04:01:41.724Z
-				mtime: 1692456789100, // 2023-08-19T11:13:09.100Z
-				size: 102400, // 100 KB
-			}),
-			createNote("3", {
-				ctime: 1672502400000, // 2023-01-01T00:00:00.000Z
-				mtime: 1675180800000, // 2023-02-01T00:00:00.000Z
-				size: 5242880, // 5 MB
-			}),
-		]) as TFile[];
-
-		// WHEN
-		const neighbours = navigator.getNeighbouringFiles(
-			files[0],
-			NeighbouringFileNavigator.ctimeSorter
-		);
-
-		// THEN
-		expectNeighbours(neighbours).toEqual(["1", "2", "3"]);
-	});
-
-	it("should sort files based on modified timestamp", () => {
-		// GIVEN
-		const files = setup([
-			createNote("2", {
-				ctime: 1700989701724, // 2023-11-26T04:01:41.724Z
-				mtime: 1692456789100, // 2023-08-19T11:13:09.100Z
-				size: 5242880, // 5 MB
-			}),
-			createNote("3", {
-				ctime: 1689876543210, // 2023-07-20T01:22:23.210Z
-				mtime: 1675180800000, // 2023-02-01T00:00:00.000Z
-				size: 102400, // 100 KB
-			}),
-			createNote("1", {
-				ctime: 1672502400000, // 2023-01-01T00:00:00.000Z
-				mtime: 1704025483489, // 2023-12-31T12:24:43.489Z
-				size: 4380, // 4.38 KB
-			}),
-		]) as TFile[];
-
-		// WHEN
-		const neighbours = navigator.getNeighbouringFiles(
-			files[0],
-			NeighbouringFileNavigator.mtimeSorter
-		);
-
-		// THEN
-		expectNeighbours(neighbours).toEqual(["1", "2", "3"]);
-	});
-
-	it("should loop folder", () => {
-		// GIVEN
-		const files = setupFiles(["1", "2", "3"]);
-		workspace.getActiveFile.mockReturnValue(files[2]);
-		settings.enableFolderLoop = true;
-
-		// WHEN
-		navigator.navigateToNextAlphabeticalFile(workspace);
-
-		// THEN
-		expect(leaf.openFile).toHaveBeenCalledWith(files[0]);
-		expect(leaf.openFile).not.toHaveBeenCalledWith(files[1]);
-		expect(leaf.openFile).not.toHaveBeenCalledWith(files[2]);
-	});
-
-	it("should not loop folders", () => {
-		// GIVEN
-		const files = setupFiles(["1", "2", "3"]);
-		workspace.getActiveFile.mockReturnValue(files[2]);
-		settings.enableFolderLoop = false;
-
-		// WHEN
-		navigator.navigateToNextAlphabeticalFile(workspace);
-
-		// THEN
-		expect(leaf.openFile).toHaveBeenCalledWith(files[2]);
-		expect(leaf.openFile).not.toHaveBeenCalledWith(files[0]);
-		expect(leaf.openFile).not.toHaveBeenCalledWith(files[1]);
-	});
-
-	it('should include specified extensions', () => {
-        // GIVEN
-        const settings: NeighbouringFileNavigatorPluginSettings = {
-            ...DEFAULT_SETTINGS,
-            includedFileTypes: 'additionalExtensions',
-            additionalExtensions: ['pdf']
-        };
-		const navigator = new NeighbouringFileNavigator(settings);
-        const files = setup([
-			createNote("1"),
-            createNote("2"),
-            createFile("3", "txt"),
-            createFile("4", "pdf"),
-        ]);
-
-        // WHEN
-        const neighbours = navigator.getNeighbouringFiles(
-			files[0] as TFile,
-			NeighbouringFileNavigator.localeSorter
-		);
-
-        // THEN
-        expectNeighbours(neighbours).toEqual(["1", "2", "4"]);
-    });
-
-    it('should include all files', () => {
-        // GIVEN
-        const settings: NeighbouringFileNavigatorPluginSettings = {
-            ...DEFAULT_SETTINGS,
-            includedFileTypes: 'allFiles'
-        };
-		const navigator = new NeighbouringFileNavigator(settings);
-        const files = setup([
-			createNote("1"),
-            createNote("2"),
-            createFile("3", "txt"),
-            createFile("4", "pdf"),
-        ]);
-
-        // WHEN
-        const neighbours = navigator.getNeighbouringFiles(
-			files[0] as TFile,
-			NeighbouringFileNavigator.localeSorter
-		);
-
-        // THEN
-        expectNeighbours(neighbours).toEqual(["1", "2", "3", "4"]);
-    });
 });
