5651f21b64302acc9b7e3ad242053ffe844eeb73
Fabian Untermoser <fabian@ederbit.xyz>
2026-05-19 21:50:18 +0200

    chore: replace .eslintignore eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "eslint/config";
 import obsidianmd from "eslint-plugin-obsidianmd";
 
 export default defineConfig([
+	{ ignores: ["node_modules/", "main.js"] },
 	...obsidianmd.configs.recommended,
 	{
 		files: ["**/*.ts"],
